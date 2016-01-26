c86ff525aab16fdd59e63ce96f6ee2d18b2f045a
Andrea Petreri <andrea.petreri@gmail.com>
2016-01-26 17:05:52 +0100

    Adding on item click listener

--- app/src/main/java/com/thetonrifles/recyclerviewsample/two/ContactsAdapter.java
+++ app/src/main/java/com/thetonrifles/recyclerviewsample/two/ContactsAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.thetonrifles.recyclerviewsample.R;
 import com.thetonrifles.recyclerviewsample.model.Contact;
@@ -14,21 +15,33 @@ import java.util.List;
 
 class ContactsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    private static class EmptyViewHolder extends RecyclerView.ViewHolder {
+    private static class EmptyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         public EmptyViewHolder(View itemView) {
             super(itemView);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(v.getContext(), "empty", Toast.LENGTH_SHORT).show();
         }
 
     }
 
-    private static class ContactViewHolder extends RecyclerView.ViewHolder {
+    private static class ContactViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView txt_full_name;
 
         public ContactViewHolder(View itemView) {
             super(itemView);
             txt_full_name = (TextView) itemView.findViewById(R.id.txt_full_name);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(v.getContext(), "item", Toast.LENGTH_SHORT).show();
         }
 
     }
